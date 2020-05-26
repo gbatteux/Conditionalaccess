5804337a332caf62152311def6008bafb7db201c
gbatteux <37621271+gbatteux@users.noreply.github.com>
2020-05-26 13:30:06 +0200

    Add files via upload

---
+++ Script/Export.ps1
@@ -0,0 +1,177 @@
+﻿$ModuleGraph = "C:\Users\GBATTEUX\Desktop\ConnectO365Services\ConnectO365Services.ps1"
+& $ModuleGraph
+
+$AccessToken = Connect-toGraph 
+
+
+################################################################################
+#################### Part for export 
+
+$exportconditionalaccess = $PSScriptRoot + ".\Condtionalaccess.csv"
+
+
+$apiUrl = 'https://graph.microsoft.com/beta/conditionalAccess/policies'
+$conditionalaccess = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+
+
+
+$allconditionalaccess = @()
+foreach ($conditionalaccess in $conditionalaccess)
+{
+    $Output = New-Object PSObject
+    $groupinclude = $null 
+    $groupexclude = $null 
+    $userInclude = $null
+    $userexclude = $null
+    $includeApplications = $null
+    $excludeApplications = $null
+    $IncludeRole = $null
+    $ExcludeRole =$null
+
+
+    ##################### part get global information 
+    $output | Add-Member NoteProperty -Name "id" -Value "$($conditionalaccess.id)"
+    $output | Add-Member NoteProperty -Name "displayname" -value "$($conditionalaccess.displayName)"
+    $output | Add-Member NoteProperty -Name "createdatetime" -value "$($conditionalaccess.createdDateTime)"
+    $output | Add-Member NoteProperty -Name "modifiedDateTime" -value "$($conditionalaccess.modifiedDateTime)"
+    $output | Add-Member NoteProperty -Name "state" -value "$($conditionalaccess.state)"
+    $output | Add-Member NoteProperty -Name "sessionControls" -value "$($conditionalaccess.sessionControls)"
+    
+    #################### part get Conditions
+    
+    # get Include App displayname 
+    $($conditionalaccess.conditions.applications.includeApplications) | % {
+        if ($_ -eq "ALL")
+        {
+            $includeApplications+= $_ +";"
+        }
+        elseif ($_ -eq "Office365")
+        {
+            $includeApplications+= $_ +";"
+        }
+        elseif ($_ -eq "none")
+        {
+            $includeApplications+= $_ +";"
+        }
+        else
+        {
+            $apiUrl = "https://graph.microsoft.com/beta/servicePrincipals?`$filter=appid eq '$_'"
+            $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+            $includeApplications+= ($Data).displayName + ";"        
+        }
+    }
+    $output | Add-Member NoteProperty -Name "includeApplications" -value  "$includeApplications"
+
+    # get exclude App displayname 
+    $($conditionalaccess.conditions.applications.excludeApplications) | % {
+        if ($_ -eq "ALL")
+        {
+            $excludeApplications+= $_ +";"
+        }
+        elseif ($_ -eq "Office365")
+        {
+            $excludeApplications+= $_ +";"
+        }
+        else
+        {
+            #$ExcludeGroupId = (Get-MsGraph -AccessToken $AccessToken -Uri $Uri | Where-Object { $_.displayName -eq $ExcludeGroup }).id
+            $apiUrl = "https://graph.microsoft.com/beta/servicePrincipals?`$filter=appid eq '$_'"
+            $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+            $excludeApplications+= ($Data).displayName + ";"        
+        }
+    }
+    $output | Add-Member NoteProperty -Name "excludeApplications" -value  "$excludeApplications"
+    $output | Add-Member NoteProperty -Name "includeUserActions" -value "$($conditionalaccess.conditions.applications.includeUserActions)"
+    $output | Add-Member NoteProperty -Name "signInRiskLevels" -value "$($conditionalaccess.conditions.signInRiskLevels)"
+    $output | Add-Member NoteProperty -Name "clientAppTypes" -value "$($conditionalaccess.conditions.clientAppTypes)"
+    $output | Add-Member NoteProperty -Name "deviceStates" -value "$($conditionalaccess.conditions.deviceStates)"
+    
+    # get Include  user displayname 
+    $($conditionalaccess.conditions.users.includeusers) | % {
+        if ($_ -eq "ALL")
+        {
+            $userInclude+= $_ +";"
+        }
+        elseif ($_ -eq "GuestsOrExternalUsers")
+        {
+            $userInclude+= $_ +";"
+        }
+        else
+        {
+            $apiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=id eq '$_'"
+            $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+            $userInclude+= ($Data).displayName + ";" 
+        
+        }
+    }
+    $output | Add-Member NoteProperty -Name "Includeusers" -value  "$userinclude"
+
+    # get Exclude user displayname 
+
+    $($conditionalaccess.conditions.users.excludeusers) | % {
+        if ($_ -eq "ALL")
+        {
+            $userexclude+= $_ +";"
+        }
+        elseif ($_ -eq "GuestsOrExternalUsers")
+        {
+            $userexclude+= $_ +";"
+        }
+        else
+        {    
+            $apiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=id eq '$_'"
+            $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+            $userexclude+= ($Data).displayName + ";"
+        }
+     }
+     $output | Add-Member NoteProperty -Name "excludeUsers" -value "$userexclude"
+
+
+    #get Include group displayname  
+    $($conditionalaccess.conditions.users.includeGroups) | % {
+    $apiUrl = "https://graph.microsoft.com/v1.0/groups?`$filter=id eq '$_'"
+    $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+    $groupInclude+= ($Data).displayName + ";" }
+    $output | Add-Member NoteProperty -Name "includeGroups" -value "$groupInclude"
+
+    #get Exclude group displayname
+    $($conditionalaccess.conditions.users.excludeGroups) | % {
+    $apiUrl = "https://graph.microsoft.com/v1.0/groups?`$filter=id eq '$_'"
+    $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+    $groupexclude+= ($Data).displayName + ";" }
+    $output | Add-Member NoteProperty -Name "excludeGroups" -value "$groupexclude"
+
+    #get Include groupManagement  displayname
+    $($conditionalaccess.conditions.users.includeRoles) | % {
+    $apiUrl = "https://graph.microsoft.com/v1.0/directoryRoleTemplates?`$filter=id eq '$_'"
+    $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+    $IncludeRole+= ($Data).displayName + ";" }
+    $output | Add-Member NoteProperty -Name "includeRoles" -value "$IncludeRole"
+    
+    #get exclude groupManagement  displayname
+    $($conditionalaccess.conditions.users.excludeRoles) | % {
+    $apiUrl = "https://graph.microsoft.com/v1.0/directoryRoleTemplates?`$filter=id eq '$_'"
+    $Data = Get-MsGraph -AccessToken $AccessToken -Uri $ApiUrl
+    $excludeRole+= ($Data).displayName + ";" }
+    $output | Add-Member NoteProperty -Name "excludeRoles" -value "$excludeRole"
+
+    $output | Add-Member NoteProperty -Name "Includeplatforms" -value "$($conditionalaccess.conditions.platforms.includePlatforms)"
+    $output | Add-Member NoteProperty -Name "excludeplatforms" -value "$($conditionalaccess.conditions.platforms.excludePlatforms)"
+    $output | Add-Member NoteProperty -Name "includeLocations" -value "$($conditionalaccess.conditions.locations.includeLocations)"
+    $output | Add-Member NoteProperty -Name "excludeLocations" -value "$($conditionalaccess.conditions.locations.excludeLocations)"
+    $output | Add-Member NoteProperty -Name "includeDeviceStates" -value "$($conditionalaccess.conditions.devices.includeDeviceStates)"
+    $output | Add-Member NoteProperty -Name "excludeDeviceStates" -value "$($conditionalaccess.conditions.devices.excludeDeviceStates)"
+    # part get grant Controls 
+    $output | Add-Member NoteProperty -Name "grantoperator" -value "$($conditionalaccess.grantControls.operator)"
+    $output | Add-Member NoteProperty -Name "grantbuiltInControls" -value "$($conditionalaccess.grantControls.builtInControls)"
+    $output | Add-Member NoteProperty -Name "customAuthenticationFactors" -value "$($conditionalaccess.grantControls.customAuthenticationFactors)"
+    $output | Add-Member NoteProperty -Name "termsOfUse" -value "$($conditionalaccess.grantControls.termsOfUse)"
+
+    $allconditionalaccess+= $Output
+
+$($conditionalaccess.displayName)
+}
+
+$allconditionalaccess | Export-Csv -Path $exportconditionalaccess -Encoding UTF8 -NoTypeInformation 
+
+
---
+++ Script/Module-graphAPI.ps1
@@ -0,0 +1,148 @@
+﻿
+function Get-MsGraph {
+    param (
+        [parameter(Mandatory = $true)]
+        $AccessToken,
+        [parameter(Mandatory = $true)]
+        $Uri
+    )
+
+    $HeaderParams = @{
+        'Content-Type'  = "application\json"
+        'Authorization' = "Bearer $AccessToken"
+    }
+
+    $ResultArray = @()
+    $Results = ""
+    $StatusCode = ""
+
+        do {
+            try {
+                $Results = Invoke-RestMethod -Headers $HeaderParams -Uri $Uri -UseBasicParsing -Method "GET" -ContentType "application/json"
+                $StatusCode = $Results.StatusCode
+            } catch {
+                $StatusCode = $_.Exception.Response.StatusCode.value__
+
+                if ($StatusCode -eq 429) {
+                    Write-Warning "Microsoft is throttling.. waiting 30 seconds."
+                    Start-Sleep -Seconds 30
+                } else {
+                    Write-Error $_.Exception
+                }
+            }
+        } while ($StatusCode -eq 429)
+            if ($Results.value) {
+                $ResultArray += $Results.value
+            } else {
+                $ResultArray += $Results
+            }
+
+        $ResultArray
+}
+
+function Post-MsGraph {
+    param (
+        [parameter(Mandatory = $true)]
+        $AccessToken,
+        [parameter(Mandatory = $true)]
+        $Uri,
+        [parameter(Mandatory = $true)]
+        $Body
+    )
+
+    $HeaderParams = @{
+        'Content-Type'  = "application\json"
+        'Authorization' = "$($OAuth.token_type) $($AccessToken)"
+    }
+
+    $ResultArray = @()
+    $Results = ""
+    $StatusCode = ""
+
+        do {
+            try {
+                $Results = Invoke-RestMethod -Headers $HeaderParams -Uri $Uri -UseBasicParsing -Method "POST" -ContentType "application/json" -Body $Body
+                $StatusCode = $Results.StatusCode
+            } catch {
+                $StatusCode = $_.Exception.Response.StatusCode.value__
+
+                if ($StatusCode -eq 429) {
+                    Write-Warning "Microsoft is throttling.. waiting 30 seconds."
+                    Start-Sleep -Seconds 30
+                } else {
+                    Write-Error $_.Exception
+                }
+            }
+        } while ($StatusCode -eq 429)
+            if ($Results.value) {
+                $ResultArray += $Results.value
+            } else {
+            $ResultArray += $Results
+        }
+    $ResultArray  
+}
+
+#region connect to Microsoft Graph
+Function Connect-toGraph {
+    # The resource URI
+$resource = "https://graph.microsoft.com"
+# Your Client ID and Client Secret obainted when registering your WebApp
+$clientid = "b3877e06-0be5-4596-8caa-1b84b18fa648"
+$clientSecret = "VL9i0YQUI-.50AhE.H6SqSSu09-L_R9z73"
+$redirectUri = "https://localhost"
+
+# UrlEncode the ClientID and ClientSecret and URL's for special characters 
+Add-Type -AssemblyName System.Web
+$clientIDEncoded = [System.Web.HttpUtility]::UrlEncode($clientid)
+$clientSecretEncoded = [System.Web.HttpUtility]::UrlEncode($clientSecret)
+$redirectUriEncoded =  [System.Web.HttpUtility]::UrlEncode($redirectUri)
+$resourceEncoded = [System.Web.HttpUtility]::UrlEncode($resource)
+$scopeEncoded = [System.Web.HttpUtility]::UrlEncode("https://outlook.office.com/user.readwrite.all")
+
+# Function to popup Auth Dialog Windows Form
+Function Get-AuthCode {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=440;Height=640}
+    $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=420;Height=600;Url=($url -f ($Scope -join "%20")) }
+
+    $DocComp  = {
+        $Global:uri = $web.Url.AbsoluteUri        
+        if ($Global:uri -match "error=[^&]*|code=[^&]*") {$form.Close() }
+    }
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($DocComp)
+    $form.Controls.Add($web)
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() | Out-Null
+
+    $queryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
+    $output = @{}
+    foreach($key in $queryOutput.Keys){
+        $output["$key"] = $queryOutput[$key]
+    }
+
+    #$output
+}
+
+
+# Get AuthCode
+$url = "https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&redirect_uri=$redirectUriEncoded&client_id=$clientID&resource=$resourceEncoded&prompt=admin_consent&scope=$scopeEncoded"
+Get-AuthCode
+# Extract Access token from the returned URI
+$regex = '(?<=code=)(.*)(?=&)'
+$authCode  = ($uri | Select-string -pattern $regex).Matches[0].Value
+
+#Write-output "Received an authCode, $authCode"
+
+
+#get Access Token
+$body = "grant_type=authorization_code&redirect_uri=$redirectUri&client_id=$clientId&client_secret=$clientSecretEncoded&code=$authCode&resource=$resource"
+$tokenResponse = Invoke-RestMethod https://login.microsoftonline.com/common/oauth2/token `
+    -Method Post -ContentType "application/x-www-form-urlencoded" `
+    -Body $body `
+    -ErrorAction STOP
+
+$tokenResponse.access_token
+}
+
